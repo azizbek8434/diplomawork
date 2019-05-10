7a18a4f79ff1a04529ba494fec3b21deeac0927a
azizbek8434 <azizbek_muzaffarov@mail.ru>
2019-05-10 15:24:45 +0500

    seeder to roles implemented

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(RegionsTableSeeder::class);
         $this->call(PermissionTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
         // $this->call(UsersTableSeeder::class);
     }
 }
---
+++ database/seeds/RolesTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('roles')->insert([
+            [
+            'name' => 'Admin',
+            'guard_name' => 'web',
+            'created_at' => new DateTime,
+            'updated_at' => new DateTime
+            ],
+            [
+            'name' => 'User',
+            'guard_name' => 'web',
+            'created_at' => new DateTime,
+            'updated_at' => new DateTime
+            ]
+        ]);
+    }
+}

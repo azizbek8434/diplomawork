63817a04a51b9d10ece652cb92192d2d19cab261
geroy111 <mr.usulidin@mail.ru>
2019-05-20 17:23:59 +0500

    window installing errors fix

--- database/factories/NeighborhoodCitizensFactory.php
+++ database/factories/NeighborhoodCitizensFactory.php
@@ -7,7 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(NeighborhoodCitizen::class, function (Faker $faker) {
     return [
-        'name' => $faker->region . ' MFY',
+        'name' => $faker->name . ' MFY',
         'region_id' => rand(1, 19)
     ];
 });

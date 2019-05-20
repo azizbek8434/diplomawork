baa2778733c31d8e1052a5eef7223dcb4a4d9188
azizbek8434 <azizbek_muzaffarov@mail.ru>
2019-05-20 17:02:47 +0500

    complait modal relations changes

--- app/Complaint.php
+++ app/Complaint.php
@@ -6,5 +6,37 @@ use Illuminate\Database\Eloquent\Model;
 
 class Complaint extends Model
 {
-    //
+    protected $fillable = [
+    'region_id',
+	'complaint_type_id',
+	'neighborhood_citizen_id',
+	'title',
+	'text',
+	'sender_full_name',
+	'sender_phone',
+	'sender_email',
+	];
+	
+	public function region()
+    {
+        return $this->belongsTo(Region::class);
+	}
+	
+	public function complaintType()
+    {
+        return $this->belongsTo(ComplaintType::class);
+	}
+
+	public function neighborhoodCitizen()
+    {
+        return $this->belongsTo(NeighborhoodCitizen::class);
+	}
+	
+	public function scopeType(Builder $builder, $type)
+    {
+        $builder->where('complaint_type_id', $type);
+    }
+
+
+	
 }

43c22ac6b9759fffd89a0618d5402bd56f899d1a
azizbek8434 <azizbek_muzaffarov@mail.ru>
2019-05-21 17:09:50 +0500

    home_controller new methods added

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Region;
+use App\Complaint;
+use App\ComplaintType;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -25,4 +28,27 @@ class HomeController extends Controller
     {
         return view('admin.dashboard');
     }
+
+    public function list(Region $region)
+    {
+        $complaint_types = ComplaintType::parents()->get();
+        return view('admin.content_type',compact('complaint_types','region'));
+    }
+    public function type($region, $type = 'aoli')
+    {
+        $region = Region::where('slug', $region)->first();
+        $complaints = Complaint::with('region')
+        ->where(function ($query) use ($region) {
+            $query->whereRegionId($region->id);
+        })->get();
+        // dd($complaints);
+        return view('admin.content_detail',compact('complaints', 'region'));
+    }
+
+    public function test(){
+        $type = ComplaintType::find(1)->with('parent')->get();
+        
+        return $type;
+    }
+
 }

dfd0445e826bd4eeb26178b41f1d653c2b68b5de
azizbek8434 <azizbek_muzaffarov@mail.ru>
2019-05-22 21:07:51 +0500

    complint factory changes

--- database/factories/ComplaintTypeFactory.php
+++ database/factories/ComplaintTypeFactory.php
@@ -3,11 +3,15 @@
 /* @var $factory \Illuminate\Database\Eloquent\Factory */
 
 use App\ComplaintType;
+use Illuminate\Support\Str;
 use Faker\Generator as Faker;
 
 $factory->define(ComplaintType::class, function (Faker $faker) {
+    $name = $faker->name;
+    $slug = Str::slug($name);
     return [
-        'name' => $faker->name,
+        'name' => $name,
+        'slug' => $slug,
         'parent_id' => rand(1, 5),
         'position' => rand(1, 10),
         'status' => rand(0, 1)

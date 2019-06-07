b0269b2b8b976593bd28810762799ec40976fcd4
azizbek8434 <azizbek_muzaffarov@mail.ru>
2019-06-07 21:17:55 +0500

    views displaying grid

--- resources/views/admin/subtypes.blade.php
+++ resources/views/admin/subtypes.blade.php
@@ -5,41 +5,38 @@
 @section('breadcrumbs', Breadcrumbs::render('content.subtype',$region, $suptype))
 
 @section('content_header')
-   <h1>
+<h1>
     {{ $suptype->name }}га тегишли суб категориялар
-    </h1>
+</h1>
 @stop
 
 @section('content')
-    <!-- Main content -->
-    <section class="content">
-      <!-- Small boxes (Stat box) -->
-      <div class="row">
-          <!-- small box -->
-        @foreach ($complaint_types as $subtype)
-            <div class="col-lg-3 col-xs-12">
-              <div class="small-box bg-aqua">
+<section class="content">
+    @foreach(array_chunk($complaint_types, 4) as $subtypes)
+    <div class="row">
+        @foreach($subtypes as $subtype)
+        <div class="col-lg-3 col-xs-12">
+            <div class="small-box bg-aqua">
                 <div class="inner">
-                  <h4>{{ $subtype['name'] }}</h4>
-              </div>
-              <div class="icon">
-                <span>{{ $subtype['complaintCount'] ?? '0' }}</span>
-              </div>
-              <a href="{{ url('dashboard/'. $region->slug .'/'.$suptype->slug.'/'.$subtype['slug']) }}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-            </div> 
-          </div>
+                    <h4>{{ $subtype['name'] }}</h4>
+                </div>
+                <div class="icon">
+                    <span>{{ $subtype['complaintCount'] ?? '0' }}</span>
+                </div>
+                <a href="{{ url('dashboard/'. $region->slug .'/'.$suptype->slug.'/'.$subtype['slug']) }}"
+                    class="small-box-footer">Барчасини кўриш <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
         @endforeach
-        <!-- ./col -->
-      </div>
-      <!-- /.row -->
-    </section>
-    <!-- /.content -->
+    </div>
+    @endforeach
+</section>
 @stop
 
 @section('css')
-    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
+{{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
 @stop
 
-@section('js')  
+@section('js')
 
 @stop

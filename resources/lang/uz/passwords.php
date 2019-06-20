ca8f42d7679519387a7a3f593f1356771f05fafd
azizbek8434 <azizbek_muzaffarov@mail.ru>
2019-06-20 22:25:20 +0500

    additions to localization files

---
+++ resources/lang/uz/passwords.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Кализ сўз камида 6 та белиган иборат бўлиши ва тасдиқланиши керак.',
+    'reset'    => 'Сизнинг калит сўзингиз тикланди!!',
+    'sent'     => 'Калит сўзни тиклаш ҳаволасини электрон почта орқали юбордик!',
+    'token'    => 'Ушбу калит сўзни қайта тиклаш белгиси (token) нотўғри.',
+    'user'     => 'Ушбу электрон почта манзилли фойдаланувчи топилмади.',
+    'reset_password_subject' => 'Паролни янгилаш',
+    'reset_password_greeting' => 'Ҳуш келибсиз :email',
+    'reset_password_line1' => 'Паролни янгилаш учун тугмани босинг',
+    'reset_password' => 'Паролни янгилаш',
+    'reset_password_line2' => 'Сайтимиздан фойдаланганиз учун рахмат'
+];

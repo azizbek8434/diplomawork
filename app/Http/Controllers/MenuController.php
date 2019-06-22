4a7357cebd7c4370d60d81719bd9c1c1d919531a
azizbek8434 <azizbek_muzaffarov@mail.ru>
2019-06-22 21:26:54 +0500

    menu comlaint count update

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -4,9 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class MenuController extends Controller
+class MenuController extends Controller 
 {
-    public function update(Request $request){
-        dd('test');
+    public function update(Request $request)
+    {
+        
+        $region = 'oltarik-tumani';
+        $count = '10';
+        if ($region && $count) {
+        foreach(config('adminlte.menu') as $key => $value){
+            if(is_array($value) && $value['can'] == $region){
+                $value['label'] = $count;
+                dump($value);
+            }
+        }
+        }
+        $fp = fopen(base_path() . '/config/adminlte.php', 'w');
+        fwrite($fp, '<?php return ' . var_export(config('adminlte'), true) . ';');
+        fclose($fp);
+        \Artisan::call('config:cache');
+        dd('done');
     }
 }

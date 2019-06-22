b00a96f1b95f42b78bbfd73297ca60a308f4e9fe
azizbek8434 <azizbek_muzaffarov@mail.ru>
2019-06-22 21:27:38 +0500

    welcom controller message store & complaint counts

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Link;
+use App\User;
 use App\Slider;
+use App\Message;
+use App\Complaint;
+use App\ComplaintType;
 use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
@@ -11,14 +15,35 @@ class WelcomeController extends Controller
     public function index(){
     $links = Link::all();
     $sliders = Slider::all();
+    $solved_complaints_count = Complaint::status()->get()->count();
+    $complaint = Complaint::all();
+    $complaint_counts = $complaint->count();
+    $complaints = $complaint->where('status', '1')->random(10);
+    $data = [
+    'complaint_counts' => $complaint_counts,
+    'solved_complaints_persentage' =>($complaint_counts * $solved_complaints_count)/100,
+    'users_count' => User::all()->count(),
+    'complaint_types' => ComplaintType::all()->count()
+    ];
 
-    return view('front.layouts.master', compact('links', 'sliders'));
+    return view('front.layouts.master', compact('links', 'sliders','data', 'complaints'));
     }
 
 
 
 
     public function message(Request $request){
-        dd($request->all());
+        
+        $this->validate($request,
+        [
+            'full_name' => 'required',
+            'email' => 'required|email',
+            'subject' => 'required',
+            'message' => 'required',
+        ]);
+
+        $input = $request->except('_token');
+        $message = Message::create($input);
+        return Response()->json(['msg'=>'OK']);
     }
 }
